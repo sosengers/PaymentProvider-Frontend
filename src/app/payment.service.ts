ec437c1a60f206c38b2f780017315d049b2f8eb9
TommasoAzz <azzalintommaso@gmail.com>
2021-04-09 16:16:46 +0200

    Aggiunto environment.example.ts

--- src/app/payment.service.ts
+++ src/app/payment.service.ts
@@ -3,13 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PaymentRequest } from './interfaces/payment-request';
 import { PaymentData } from './interfaces/payment-data';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
   
-  paymentProviderBackendURL: string = "http://0.0.0.0:4001"
+  paymentProviderBackendURL: string = environment.paymentProviderBackend;
 
   constructor(private http: HttpClient) { }
 
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  paymentProviderBackend: '',
   production: true
 };
